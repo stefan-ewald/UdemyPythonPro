a7d6fae9499b77ed2ff86a69d0261c9e26167f31
franneck94 <schaffranek.jan@web.de>
2021-08-12 14:46:59 +0200

    Updated code

--- Chapter3_BasicFeatures/Scope/local_and_global_scope.py
+++ Chapter3_BasicFeatures/Scope/local_and_global_scope.py
@@ -1,11 +1,19 @@
 from my_lib import GLOBAL_MYLIB_VAR
 
 
-my_global_int = 0.0
+global_int = 0.0
+
+
+def fun():
+    a = 2
 
 
 def main():
-    my_local_int = 3
+    if True:
+        b = 2
+    local_int = 3
+    print(local_int)
+    print(global_int)
     print(GLOBAL_MYLIB_VAR)
 
     print(f"Locals:\n{locals()}")
