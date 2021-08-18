10792757eead97f134dcf44c4514cc9423b3933b
franneck94 <schaffranek.jan@web.de>
2021-08-18 18:38:23 +0200

    up

--- Chapter7_CodingGuidelines/TypeAnnotations/more_to_type_annotations3.py
+++ Chapter7_CodingGuidelines/TypeAnnotations/more_to_type_annotations3.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import Container
+from typing import Any, Container
 from typing import Iterable
 from typing import Sized
 from typing import Collection
@@ -9,16 +9,20 @@ from typing import MutableSequence
 from typing import Protocol
 
 
-class SizedIterable(Protocol):
+class SizedMutableIterable(Protocol):
     def __len__(self):
         pass
 
     def __getitem__(self, i: int):
         pass
 
+    def __setitem__(self, i: int, v: Any):
+        pass
 
-def iterate_over_length(obj: SizedIterable) -> None:
+
+def iterate_over_length(obj: SizedMutableIterable) -> None:
     for i in range(len(obj)):
+        obj[i] = i**2
         print(obj[i])
 
 
