16a9987f6e4320514c999035f2c3d45ff1e90edc
franneck94 <schaffranek.jan@web.de>
2021-12-16 11:38:42 +0100

    up

--- Chapter7_CodingGuidelines/TypeAnnotations/new_type_var.py
+++ Chapter7_CodingGuidelines/TypeAnnotations/new_type_var.py
@@ -1,41 +1,26 @@
 from __future__ import annotations
 
 from typing import NewType
-from typing import TypeVar
-from typing import Union
 
 
-UserId_NT = NewType('UserId_NT', str)
-UserId_TV = TypeVar('UserId_TV', str, bytes)
-UserId = Union[str, bytes]
-UserId2 = str
+UserId1 = NewType('UserId1', int)
+UserId2 = int
 
 
-def print_text1(text: UserId_NT) -> None:
-    print(text)
+def name_by_id1(user_id: UserId1) -> None:
+    print(user_id)
 
 
-def print_text2(text: UserId_TV) -> None:
-    print(text)
+def name_by_id2(user_id: UserId2) -> None:
+    print(user_id)
 
 
-def print_text3(text: UserId) -> None:
-    print(text)
+def main() -> None:
+    name_by_id1(UserId1(42))
+    name_by_id1(UserId2(42))  # NewType => int
 
-
-def main():
-    u1 = 'hello'
-    u2 = b'hello'
-
-    # print_text1(u1)  # error
-    # print_text1(u2)  # error
-    print_text1(UserId_NT(u1))  # okay
-
-    print_text2(u1)
-    print_text2(u2)
-
-    print_text3(u1)
-    print_text3(u2)
+    name_by_id2(UserId1(42))  # int => NewType
+    name_by_id2(UserId2(42))
 
 
 if __name__ == '__main__':
