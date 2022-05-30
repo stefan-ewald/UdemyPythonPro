2a84431b81282ba60e3a813aa2e7e6f86f2c0635
franneck94 <schaffranek.jan@web.de>
2022-05-30 13:44:45 +0200

    up

---
+++ Chapter7_CodingGuidelines/TypeAnnotations/args_kwargs.py
@@ -0,0 +1,20 @@
+from __future__ import annotations
+
+from typing import Any
+from typing import Callable
+
+
+def f(*args: int, **kwargs: int) -> None:
+    print(f"{args}, {kwargs}")
+
+
+def call(f: Callable[..., None], *args: int, **kwargs: int) -> None:
+    return f(*args, **kwargs)
+
+
+def main() -> None:
+    call(f, 1, 2, 3, a=1, b=2, c=3)
+
+
+if __name__ == "__main__":
+    main()
