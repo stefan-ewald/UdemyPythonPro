4a7a666f88196faeeb7d5b5dce3aac6f3ffc7912
Stefan Ewald <algonquin@gmx.net>
2022-07-03 12:22:49 +0200

    wip

--- own/decorators.py
+++ own/decorators.py
@@ -6,3 +6,13 @@ def do_twice(func):
         func(*args, **kwargs)
         return func(*args, **kwargs)
     return wrapper_do_twice
+
+def repeat(num_times):
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            for _ in range(num_times):
+                value = func(*args, **kwargs)
+            return value
+        return wrapper
+    return decorator
